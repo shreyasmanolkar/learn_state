0d9becc88870d63330c3bd1952519a3fb7c86f92
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-19 22:44:47 +0530

    in-direct state management

--- in_direct_management/src/App.tsx
+++ in_direct_management/src/App.tsx
@@ -1,17 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { usePokemon, PokemonProvider } from "./store";
+import { Link, Outlet, ReactLocation, Router, useMatch } from "@tanstack/react-location";
 
 // -------------------------------------------------- component ----------------------------------------------------------
 
 const queryClient = new QueryClient();
+const location = new ReactLocation();
 
 const PokemonList = () => {
   const {pokemon} = usePokemon();
   return(
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-3">
       {pokemon.map((p) => (
-        <li
+        <Link
           key={p.id}
+          to={`/pokemon/${p.id}`}
+        >
+        <li
           className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
         >
           <div className="flex-1 flex flex-col p-8">
@@ -23,6 +28,7 @@ const PokemonList = () => {
             <h3 className="mt-6 text-gray-900 text-sm font-medium">{p.name}</h3>
           </div>
         </li>
+        </Link>
       ))}
     </ul>
   )
@@ -42,17 +48,90 @@ function SearchBox(){
   )
 };
 
+function PokemonDetail(){
+
+  const {
+    params: {id}
+  } = useMatch();
+
+  const { pokemon } = usePokemon();
+
+  const pokemonData = pokemon.find((p) => p.id === +id);
+
+  if(!pokemonData){
+    return <div>No pokemon found</div>
+  }
+
+  return(
+    <div className="mt-3">
+      <Link to="/">
+        <h1 className="text-2xl font-bold mb-5">&lt; Home</h1>
+      </Link>
+      <div className="grid grid-cols-2">
+        <img
+          className="w-96 h-96 flex-shrink-0 mx-auto bg-black rounded-xl"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`}
+          alt=""
+        />
+        <div className="ml-3">
+          <h2 className="text-2xl font-bold">{pokemonData.name}</h2>
+          <div className="mt-3">
+            <h3 className="text-xl font-bold">Stats</h3>
+            <ul className="mt-3">
+              {[
+                "hp",
+                "attack",
+                "defense",
+                "special_attack",
+                "special_defense",
+                "speed",
+              ].map((stat) => (
+                <li key={stat} className="grid grid-cols-2">
+                  <span className="font-bold">{stat}</span>
+                  <span>{pokemonData[stat as keyof typeof pokemonData]}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 // -------------------------------------------------- app ----------------------------------------------------------
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <SearchBox/>
+        <PokemonList/>
+      </>
+    ),
+  },
+  {
+    path: "/pokemon/:id",
+    element: (
+      <>
+        <PokemonDetail/>
+      </>
+    ),
+  }
+]
+
 
 function App() {  
 
   return (
     <QueryClientProvider client={queryClient}>
       <PokemonProvider>
-        <div className="mx-auto max-w-3xl">
-          <SearchBox/>
-          <PokemonList/>
-        </div>
+        <Router location={location} routes={routes}>
+          <div className="mx-auto max-w-3xl">
+            <Outlet />
+          </div>
+        </Router>
       </PokemonProvider>
     </QueryClientProvider>
   );
